2275d574020e1eb613da811f26a0170889c4ba12
Dao Quang Chinh <chinhdqhe171746@fpt.edu.vn>
2024-05-29 11:47:39 +0700

    forgotpassword

--- src/java/modal/user.java
+++ src/java/modal/user.java
@@ -8,6 +8,103 @@ package modal;
  *
  * @author Dell
  */
-public class user {
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String fullname;
+    private String gender;
+    private int phone;
+    private String email;
+    private String img;
+    private int roleId;
+
+    public User() {
+    }
+
+    public User(String id, String username, String password, String fullname, String gender, int phone, String email, String img, int roleId) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.gender = gender;
+        this.phone = phone;
+        this.email = email;
+        this.img = img;
+        this.roleId = roleId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+    
     
 }
